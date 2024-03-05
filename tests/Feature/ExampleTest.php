300ccfb2aca025bbaeaaad280c11db5af886b702
Matheus Bezerra <Matheussuarao@gmail.com>
2024-03-05 13:11:46 -0300

    Atualizando Composer

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -12,8 +12,11 @@ class ExampleTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response(): void
     {
+
+        
         $response = $this->get('/');
 
-        $response->assertStatus(200);
+        $response->assertStatus(302)
+        ->assertRedirect(route('login'));
     }
 }
